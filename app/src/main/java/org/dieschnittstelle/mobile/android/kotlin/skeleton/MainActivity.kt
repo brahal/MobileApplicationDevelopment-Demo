0dea35a789cdd7dde06265277de0594af1ffb68d
rahbasm <basma.rahal@stud.th-luebeck.de>
2026-01-11 15:10:14 +0100

    Test commit

--- app/src/main/java/org/dieschnittstelle/mobile/android/kotlin/skeleton/MainActivity.kt
+++ app/src/main/java/org/dieschnittstelle/mobile/android/kotlin/skeleton/MainActivity.kt
@@ -1,20 +1,9 @@
 package org.dieschnittstelle.mobile.android.kotlin.skeleton
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -22,7 +11,6 @@ import androidx.navigation.toRoute
 import org.dieschnittstelle.mobile.android.kotlin.skeleton.model.MediaItem
 import org.dieschnittstelle.mobile.android.kotlin.skeleton.view.OverviewScreen
 import org.dieschnittstelle.mobile.android.kotlin.skeleton.model.RoomLocalMediaItemCRUDOperationsImpl
-import org.dieschnittstelle.mobile.android.kotlin.skeleton.model.createRandomMediaItem
 import org.dieschnittstelle.mobile.android.kotlin.skeleton.ui.theme.MADDemoTheme
 import org.dieschnittstelle.mobile.android.kotlin.skeleton.view.MapScreen
 import org.dieschnittstelle.mobile.android.kotlin.skeleton.view.ReadviewScreen
